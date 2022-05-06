0b763c2ebbb62f505ee5f67e339d5f01a5a47346
jong <qkr7627@naver.com>
2022-05-07 00:32:43 +0900

    first commit

---
+++ app/src/main/java/com/example/testforgit/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.testforgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //개발 리더 공간
+
+
+
+
+
+
+        // 신입 사원 공간
+
+
+    }
+}
